645d028f8f1744e068103fe514ed31805fd92f00
Gubbala Jayakumar <115056837+jayak0776@users.noreply.github.com>
2025-07-28 15:35:35 +0530

    err

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-
 require("dotenv").config();
 
-//db
 const connectDB = require("./config/db");
-connectDB(); // then connect
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-  "http://localhost:5173", // for local dev
-];
+// Allow only local frontend
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-// Middlewares
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
 app.use(express.json());
-
 
 // Routes
 const routes = require("./routes/routes");
 app.use("/api", routes);
 
-// Root
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
